2c54a90c6de0a361f528caefa851dce0ee2e209a
Corey Carter <crc2231@gmail.com>
2020-07-14 10:04:15 -0600

    Add renters method

--- test/building_test.rb
+++ test/building_test.rb
@@ -6,16 +6,17 @@ class BuildingTest < Minitest::Test
 
   def test_it_exists
     building = Building.new
+
     assert_instance_of Building, building
   end
 
   def tests_units_empty
     building = Building.new
+
     assert_equal [], building.units
   end
 
   def test_it_can_add_units
-
     building = Building.new
     unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
@@ -23,6 +24,35 @@ class BuildingTest < Minitest::Test
     building.add_unit(unit2)
 
     assert_equal [unit1, unit2], building.units
+  end
+
+  def test_it_starts_with_no_renters
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    assert_equal [], building.renters
+  end
+
+  def test_it_can_add_renters
+    building = Building.new
+
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+
+    renter1 = Renter.new("Aurora")
+    unit1.add_renter(renter1)
+
+    assert_equal ["Aurora"], building.renters
+
+    renter2 = Renter.new("Tim")
+    unit2.add_renter(renter2)
+
+    assert_equal ["Aurora", "Tim"], building.renters
   end
 
 end
