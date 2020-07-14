63ebd2645ca59f8974b378ef959d4519a6dd2c69
Corey Carter <crc2231@gmail.com>
2020-07-14 10:04:34 -0600

    Add renters method

--- lib/building.rb
+++ lib/building.rb
@@ -2,12 +2,23 @@ require './lib/apartment'
 
 class Building
 attr_reader :units
+
   def initialize
     @units = []
   end
 
   def add_unit(unit)
     @units << unit
+  end
+
+  def renters
+    renter_array = []
+    @units.each do |unit|
+      if unit.renter != nil
+        renter_array << unit.renter.name
+      end
+    end
+    renter_array
   end
 
 end
