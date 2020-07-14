2b862cd1a63bccb85a14ff57a1750cdc23e939d7
Corey Carter <crc2231@gmail.com>
2020-07-14 09:11:42 -0600

    Complete initial renter class and test setup

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -5,7 +5,13 @@ require './lib/renter'
 class RenterTest < Minitest::Test
 
   def test_it_exists
-    renter = Renter.new("Corey")
+    renter = Renter.new("Jessie")
     assert_instance_of Renter, renter
   end
+
+  def test_it_has_a_name
+    renter = Renter.new("Jessie")
+    assert_equal "Jessie", renter.name
+  end
+
 end
